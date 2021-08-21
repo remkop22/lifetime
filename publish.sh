a70ba68fb1fa847bcc0555a931ad563f8f22ff9f
Jonas Jebing <noreply.jonas.8049@protonmail.com>
2021-08-21 12:03:38 +0200

    0.1.0

--- publish.sh
+++ publish.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 set -x
-set -e
 
 cd "${0%/*}"
 
 cd workspace/proc_macros_impl
 cargo publish
+sleep 5s
 cd ../proc_macros
 cargo publish
+sleep 5s
 cd ../..
 cargo publish
+sleep 5s
